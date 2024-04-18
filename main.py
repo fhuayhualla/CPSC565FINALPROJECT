87802f37fc753e5b3e14a9097c9367d5a18e8b1f
fhuayhualla <fabriciohuayhualla@gmail.com>
2024-04-18 17:25:55 -0600

    I have implemented the overall structure of the model, meaning that cars now drive in a dynamic approach. I still need to add more distinct rules for the different driver types

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ BUTTON_COLOR = (100, 200, 255)
 BUTTON_HOVER_COLOR = (100, 100, 255)
 TEXT_COLOR = (255, 255, 255)
 FONT_SIZE = 20
-CAR_COLORS = ['red', 'green', 'blue', 'yellow', 'purple']
+CAR_COLORS = ['red', 'yellow', 'green']
 CAR_WIDTH = 40
 CAR_HEIGHT = 20
 ROAD_COLOR = (50, 50, 50)
 DASH_COLOR = (255, 255, 255)
 DASH_LENGTH = 20
 DASH_SPACE = 20
+ROAD_TOP = 150
 
 
 pygame.init()
@@ -23,6 +24,47 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, FONT_SIZE)
 
+
+class Car:
+    def __init__(self, x, y, speed, color):
+        self.x = x
+        self.y = y
+        self.base_speed = speed
+        self.current_speed = speed
+        self.color = color
+        self.target_y = y
+        self.is_changing_lanes = False
+        self.lane_change_cooldown = 0
+
+    def adjust_speed(self, other_cars):
+        min_distance = float('inf')
+        for other in other_cars:
+            if self.y == other.y and other.x > self.x:
+                distance = other.x - self.x
+                if distance < min_distance:
+                    min_distance = distance
+        if min_distance < 100:
+            self.current_speed = max(1, min(self.current_speed, other.current_speed - 1))
+        else:
+            self.current_speed = self.base_speed
+
+    def move(self):
+        self.x += self.current_speed
+        if self.x > SCREEN_WIDTH:
+            self.x = -CAR_WIDTH
+        if self.y != self.target_y:
+            vertical_step = 2 if self.target_y > self.y else -2
+            self.y += vertical_step
+            if abs(self.target_y - self.y) < 2:
+                self.y = self.target_y
+                self.is_changing_lanes = False
+                if self.lane_change_cooldown == 0:
+                    self.lane_change_cooldown = 60
+        if self.lane_change_cooldown > 0:
+            self.lane_change_cooldown -= 1
+
+    def draw(self):
+        pygame.draw.rect(screen, pygame.Color(self.color), (self.x, self.y - CAR_HEIGHT // 2, CAR_WIDTH, CAR_HEIGHT))
 
 class Button:
     def __init__(self, x, y, width, height, text):
@@ -32,7 +74,6 @@ class Button:
 
     def draw(self):
         action = False
-
         pos = pygame.mouse.get_pos()
         if self.rect.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
@@ -47,59 +88,88 @@ class Button:
         return action
 
 
-running = True
-simulation_active = False
-num_lanes = 3
-cars = []
 
 setup_button = Button(50, 50, 100, 50, 'Setup')
 go_button = Button(200, 50, 100, 50, 'Go')
 lane_buttons = [Button(400 + i * 110, 50, 100, 50, f'{n} Lanes') for i, n in enumerate([3, 5, 7])]
 
 
+running = True
+simulation_active = False
+num_lanes = 3
+cars = []
+
+
 def create_cars():
     global cars, num_lanes
     cars.clear()
-    road_top = 150
-    lane_height = (SCREEN_HEIGHT - road_top) / num_lanes
+    lane_height = (SCREEN_HEIGHT - ROAD_TOP) / num_lanes
+
     for lane in range(num_lanes):
-        y = road_top + (lane + 0.5) * lane_height
-        for _ in range(5):
-            x = random.randint(50, SCREEN_WIDTH - 50)
-            speed = random.randint(1, 5)
+        y = ROAD_TOP + (lane + 0.5) * lane_height
+        last_x = 50
+        while True:
+            next_min_x = last_x + 100
+            next_max_x = min(SCREEN_WIDTH - 50, next_min_x + 200)
+            if next_min_x >= SCREEN_WIDTH - 50:
+                break
+            if next_min_x > next_max_x:
+                break
+            x = random.randint(next_min_x, next_max_x)
             color = random.choice(CAR_COLORS)
-            cars.append((x, y, speed, color))
+            speed = 4 if color == 'red' else (3 if color == 'yellow' else 2)
+            car = Car(x, y, speed, color)
+            cars.append(car)
+            last_x = x
+
+
+
 
 
 def move_cars():
-    for idx, (x, y, speed, color) in enumerate(cars):
-        x += speed
-        if x > SCREEN_WIDTH:
-            x = -CAR_WIDTH
-        cars[idx] = (x, y, speed, color)
+    global cars, num_lanes
+    lane_height = (SCREEN_HEIGHT - ROAD_TOP) / num_lanes
+    sorted_cars = sorted(cars, key=lambda c: (c.y, c.x))
+
+    for i, car in enumerate(sorted_cars):
+        car.adjust_speed(sorted_cars)
+        if not car.is_changing_lanes and car.lane_change_cooldown == 0:
+            for other in sorted_cars:
+                if other.y == car.y and 0 < other.x - car.x < 80 and other.current_speed < car.base_speed:
+                    possible_lanes = [l for l in range(num_lanes) if l != int((car.y - ROAD_TOP) / lane_height)]
+                    for new_lane in possible_lanes:
+                        new_y = ROAD_TOP + (new_lane + 0.5) * lane_height
+                        if all(not (abs(other.y - new_y) < 5 and abs(other.x - car.x) < CAR_WIDTH + 50) for other in sorted_cars):
+                            car.target_y = new_y
+                            break
+
+        car.move()
+
 
 
 def draw_cars():
-    for x, y, speed, color in cars:
-        pygame.draw.rect(screen, pygame.Color(color), (x, y - CAR_HEIGHT // 2, CAR_WIDTH, CAR_HEIGHT))
+    for car in cars:
+        car.draw()
 
 
 def draw_road():
-    road_top = 150
-    pygame.draw.rect(screen, ROAD_COLOR, (0, road_top, SCREEN_WIDTH, SCREEN_HEIGHT - road_top))
-    lane_height = (SCREEN_HEIGHT - road_top) / num_lanes
+    pygame.draw.rect(screen, ROAD_COLOR, (0, ROAD_TOP, SCREEN_WIDTH, SCREEN_HEIGHT - ROAD_TOP))
+
+    lane_height = (SCREEN_HEIGHT - ROAD_TOP) / num_lanes
+
     for lane in range(1, num_lanes):
-        y = road_top + lane * lane_height
+        y = ROAD_TOP + lane * lane_height
         for x in range(0, SCREEN_WIDTH, DASH_LENGTH + DASH_SPACE):
             pygame.draw.rect(screen, DASH_COLOR, (x, y - 2, DASH_LENGTH, 4))
 
 
 while running:
     screen.fill((30, 30, 30))
+
     draw_road()
 
     for event in pygame.event.get():
-        if event.type is pygame.QUIT:
+        if event.type == pygame.QUIT:
             running = False
 
     if setup_button.draw():
@@ -119,8 +189,10 @@ while running:
         move_cars()
 
     draw_cars()
+
     pygame.display.flip()
     clock.tick(60)
-#test pycharm
+
 pygame.quit()
 sys.exit()
+
